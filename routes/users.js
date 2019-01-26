5515b0d6bf75d451dce66a87095295b206c59fa8
Mark <meichangliang@outlook.com>
2019-01-27 01:42:03 +0800

    'add'

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+let url = require('url');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(`respond with a resource,${res.path}`);
+  res.send(`respond with a resource,${req._parsedOriginalUrl.href}`);
 });
 
 module.exports = router;
