6098b3d750e2f47df7a8ee23fb2d2be75c20d6f7
Mark <meichangliang@outlook.com>
2019-01-27 01:26:24 +0800

    'add'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+const data = {
+  name: '梅昌亮',
+  age: '24'
+};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express,is very good' });
+  res.render('index', {
+    title: 'Express',
+    body: 'Express is very good',
+    data
+  });
 });
 
 module.exports = router;
