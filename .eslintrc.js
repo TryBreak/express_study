a67dc95cbe9e57fe2b185e22a45d9bbe28a03640
Mark <meichangliang@outlook.com>
2019-01-27 00:29:53 +0800

    AdD

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
-  "extends": "eslint:recommended",
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		node: true
+	},
+	extends: 'eslint:recommended',
+	parserOptions: {
+		ecmaVersion: 2016,
+		sourceType: 'module'
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always']
+	}
 };
-
