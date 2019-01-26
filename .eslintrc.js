ef73b593379c88dfc7d66e05d72b64e4a494d4c5
Mark <meichangliang@outlook.com>
2019-01-27 00:19:55 +0800

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,4 @@
 module.exports = {
-  env: {
-    browser: true, es6: true,
-  },
-  extends: ['react-app', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaFeatures: { jsx: true, },
-    ecmaVersion: 2018, sourceType: 'module',
-  },
   "extends": "eslint:recommended",
-  plugins: ['react'],
-  rules: {
-    'linebreak-style': ['error', 'unix'],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'prettier/prettier': 'warn',
-      quotes: ['error', 'single'],
-  },
 };
 
