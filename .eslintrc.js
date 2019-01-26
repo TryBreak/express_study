89c2d83dc8e670fb402c3c550ed5fa36783d2ab8
Mark <meichangliang@outlook.com>
2019-01-27 01:20:28 +0800

    'add'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-	env: {
-		browser: true,
-		commonjs: true,
-		es6: true,
-		node: true
-	},
-	extends: 'eslint:recommended',
-	parserOptions: {
-		ecmaVersion: 2016,
-		sourceType: 'module'
-	},
-	rules: {
-		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-		'no-unused-vars': [0]
-	}
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: "module"
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "no-unused-vars": [0]
+  }
 };
